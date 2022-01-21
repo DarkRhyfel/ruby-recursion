0935043be708ddbd5751f507cba0fa51bc1b91ce
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-21 17:12:42 -0500

    Added fibonacci recursive
    Added fibonaccio recursive implementation.

--- Fibonacci/main.rb
+++ Fibonacci/main.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-def fibs_aux(n)
+def fibs_aux(num)
   previous = 0
   result = 1
 
-  (2..n).each do |_|
+  (2..num).each do |_|
     temp = result
     result += previous
     previous = temp
@@ -13,13 +13,23 @@ def fibs_aux(n)
   result
 end
 
-def fibs(n)
-  case n
+def fibs(num)
+  case num
   when 0 then 0
   when 1 then 1
   else
-    fibs_aux(n)
+    fibs_aux(num)
   end
 end
 
-puts fibs(6)
+puts fibs(10)
+
+def fibs_rec_aux(num, next_num, current)
+  num.zero? ? current : fibs_rec_aux(num - 1, next_num + current, next_num)
+end
+
+def fibs_rec(num)
+  fibs_rec_aux(num, 1, 0)
+end
+
+puts fibs_rec(10)
